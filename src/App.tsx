6b1a4351dcc8ac81e3c6674cfa3b809a48779e0f
Edwinfpirajan <efpa1998@hotmail.com>
2023-09-25 23:38:42 -0500

    lll

--- src/App.tsx
+++ src/App.tsx
@@ -4,41 +4,47 @@ import viteLogo from '/vite.svg'
 import { ReactCompareSlider, ReactCompareSliderImage } from 'react-compare-slider';
 import ImageOne from './assets/ImageOne.jpg'
 import ImageTwo from './assets/ImageTwo.jpg'
+import Antena from './assets/antena.gif'
 import './App.css'
 
 function App() {
 
   return (
     <>
-    <div className='login'>
-    <div className="login-box">
-  <h2>Ingreso</h2>
-  <form>
-    <div className="user-box">
-      <input type="text" name="" />
-      <label>Usuario</label>
-    </div>
-    <div className="user-box">
-      <input type="password" name=""/>
-      <label>Contraseña</label>
-    </div>
-    <a href="#">
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      Submit
-    </a>
-  </form>
-</div>
-    </div>
-      <div className="contenido" style={{ width: "100%", height: "100vh", display:"flex", flexDirection:"column", alignItems:"start", paddingTop:"30px" }}>
-        <h1 style={{textAlign:"center"}}>Con tu guía, conquistamos cualquier desafío</h1>
-        <ReactCompareSlider
-          itemOne={<ReactCompareSliderImage src={ImageOne} style={{height:"100%", width:"100%"}}/>}
-          itemTwo={<ReactCompareSliderImage src={ImageTwo}  style={{height:"100%", width:"100%"}}/>}
-        />
-
+      <div className='login'>
+        <div className="login-box">
+          <h2>Ingreso</h2>
+          <form>
+            <div className="user-box">
+              <input type="text" name="" />
+              <label>Usuario</label>
+            </div>
+            <div className="user-box">
+              <input type="password" name="" />
+              <label>Contraseña</label>
+            </div>
+            <a href="#">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              ENTRAR
+            </a>
+          </form>
+        </div>
+      </div>
+      {/* _________________ */}
+      <div className="carta-box">
+      </div>
+      <div className="container" style={{ width: "100%", height: "100vh", display: "flex", flexDirection: "column", alignItems: "center", paddingTop: "60px", }}>
+        <h1 style={{ textAlign: "center" }}>Con tu guía, conquistamos cualquier desafío</h1>
+        <div className='gallery'>
+          <ReactCompareSlider
+            itemOne={<ReactCompareSliderImage src={ImageOne} style={{ height: "100%", width: "100%" }} />}
+            itemTwo={<ReactCompareSliderImage src={ImageTwo} style={{ height: "100%", width: "100%" }} />}
+          />
+        </div>
+        <img src={Antena} className='antena' />
       </div>
 
     </>
